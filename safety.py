cb05c3edc331b9c4a9103b88819c3a990d9fedcd
Elias Christensson <Elias.c.christensson@gmail.com>
2026-02-01 23:18:41 +0100

    Forum improvments
    Added design for the forum, and new topics page was added aswell as topics

--- safety.py
+++ safety.py
@@ -1,6 +1,3 @@
-import time
-import logging
-import secrets
 from functools import wraps
 from collections import defaultdict
 
@@ -9,4 +6,3 @@ from flask_limiter.util import get_remote_address
 
 # Skappar en "rate limiter" som spårar användare genom deras IP-address 
 limiter = Limiter(key_func=get_remote_address)
-login_attempts = defaultdict(list)
