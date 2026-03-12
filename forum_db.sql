6f3620860b1312a341d6d745e2138b6573d46e76
Maliketh_blackblade <Jeff.janers@gmail.com>
2026-03-12 12:27:29 +0100

    Update forum_db.sql
    Updated database with A_I and unique in users

--- forum_db.sql
+++ forum_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 06 mars 2026 kl 09:29
+-- Tid vid skapande: 12 mars 2026 kl 12:26
 -- Serverversion: 10.4.32-MariaDB
--- PHP-version: 8.0.30
+-- PHP-version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -83,8 +83,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`) VALUES
-(1, '', 'test', '', 'scrypt:32768:8:1$OMxsVxOfU3Nh6KRi$3ee295a957fbb582a36d2ed4454b5984307d338ca5aeb8d2d5968ce1fa07ef48f41755bf961e63957a07c323ef18c2c89c1bac23acfa6d3fd306455286d3333b'),
-(0, '', 'Admin', '', 'scrypt:32768:8:1$K2dXepR8Vnk5Oihd$eaf8b6e0b8e9b3d77437419ab65344b6fd3b604f7d965bb1eb07f0f2e17b18471df5ebac5c8d4ebe7b51b725d82362e1008a186c0fcf7be8ec698c421efe3d2c');
+(1, 'test', 'test', 'test.test@gmail.com', 'scrypt:32768:8:1$dnxRXVfv1s3cH2zn$675baf74d16fc5264f62932f9ddbe0c30f83b34e2cf647626a7ea1c06d8c98cf1d9b547e6530081112464dcc196aad1b625003e7c9dd532081c0b67271685662'),
+(2, 'Sosse', 'Magdalena', 'downie.ss@hotmail.com', 'scrypt:32768:8:1$HPVmenSDLyWxVrWw$06efb48cd5a941ddb551b8567452e693c00ce0a7562caea817d4689372df7beae9dbc06226e2277617097b56c0a7f717ed5aa2497540a198f3ec4b28d207e3d2');
 
 --
 -- Index för dumpade tabeller
@@ -103,6 +103,13 @@ ALTER TABLE `topics`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
@@ -117,6 +124,12 @@ ALTER TABLE `posts`
 --
 ALTER TABLE `topics`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT för tabell `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
