c57330ecc448bcb190df84ab628bf3a6c83ed11f
Elias Christensson <Elias.c.christensson@gmail.com>
2026-03-18 16:00:46 +0100

    skit

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,1 @@
-function toggleDarkMode() {
-   document.body.classList.toggle("dark-mode");
-   document.footer.classList.toggle("dark-mode");
-   document.header.classList.toggle("dark-mode");
-   localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
-}
+
